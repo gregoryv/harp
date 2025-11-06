96dd22dcca2b5ea6fb647515b07bcd843f8ad96a
Gregory Vincic <gregoryvse@gmail.com>
2025-11-06 21:07:24 +0100

    Refactor

--- cmd/warp/main.go
+++ cmd/warp/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -21,7 +20,7 @@ func main() {
 	targetIP := flag.String(
 		"ip", "", "arp IP range, e.g 192.1.1.3-128 or 192.1.1.*",
 	)
-	verbose := flag.Bool("verbose", false, "debug logs")
+	verbose := flag.Bool("verbose", false, "")
 	flag.Parse()
 
 	if *verbose {
@@ -74,80 +73,4 @@ func main() {
 		}
 		fmt.Println(mac, ip)
 	}
-}
-
-func showInterfaces(available []net.Interface) {
-	fmt.Println("# available interfaces")
-	for _, iface := range available {
-		addr, _ := findIP4(&iface)
-		ip := ipstr(addr)
-		fmt.Fprintf(os.Stderr, "%s %s\n", iface.Name, ip)
-	}
-}
-
-// listInterfaces returns a filtered list of interfaces that may be
-// scanned.
-func listInterfaces() []net.Interface {
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var available []net.Interface
-	for _, iface := range ifaces {
-		addr, _ := findIP4(&iface)
-		ip := ipstr(addr)
-		if ip == "" {
-			continue
-		}
-		if strings.HasPrefix(ip, "172.") {
-			continue
-		}
-		available = append(available, iface)
-	}
-	return available
-}
-
-func ipstr(addr *net.IPNet) string {
-	if addr == nil {
-		return ""
-	}
-	v := addr.String()
-	i := strings.Index(v, "/")
-	if i < 0 {
-		return v
-	}
-	return v[:i]
-}
-
-func findIP4(iface *net.Interface) (*net.IPNet, error) {
-	var addr *net.IPNet
-	addrs, err := iface.Addrs()
-	if err != nil {
-		return nil, err
-	}
-	for _, a := range addrs {
-		if ipnet, ok := a.(*net.IPNet); ok {
-			if ip4 := ipnet.IP.To4(); ip4 != nil {
-				addr = &net.IPNet{
-					IP:   ip4,
-					Mask: ipnet.Mask[len(ipnet.Mask)-4:],
-				}
-				break
-			}
-		}
-	}
-	// Sanity-check that the interface has a good address.
-	switch {
-	case addr == nil:
-		return nil, fmt.Errorf("%s: not found", iface.Name)
-
-	case addr.IP[0] == 127:
-		return nil, errors.New("is localhost")
-
-	case addr.Mask[0] != 0x_ff || addr.Mask[1] != 0xff:
-		return nil, errors.New("mask too large")
-	}
-
-	return addr, nil
 }
